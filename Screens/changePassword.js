562df4895188014d5d99a7bebb1af0328ce2906b
mahinour30 <mahinour1233@gmail.com>
2020-10-13 01:35:17 +0200

    Update changePassword.js

--- Screens/changePassword.js
+++ Screens/changePassword.js
@@ -1,17 +1,85 @@
 
 import React from 'react';
-import {View,Text,StyleSheet, Dimensions,TextInput,TouchableOpacity, Button} from 'react-native';
+import {View,Alert,Text,StyleSheet, Dimensions,TextInput,TouchableOpacity, Button} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Feather from 'react-native-vector-icons/Feather';
 import {useTheme} from '@react-navigation/native';
+import { ScrollView } from 'react-native-gesture-handler';
+import firestore from '@react-native-firebase/firestore';
+import SignUpScreen from './SignUpScreen'
+
+const ChangePasswordScreen= (props) => {
+
+  const ID=props.userID
+
+  const [data, setData] = React.useState({
+    current_password:'',
+    confirm_password:'',
+    new_password:'',
+    
+    
+});
 
 
-const ChangePasswordScreen= ({navigation}) => {
+const handleCurrentPassordChange=(val) =>{
+  setData({
+    ...data,
+    current_password:val,
+  });
+};
+
+
+const handleConfirmPasswordChange=(val) =>{
+  setData({
+    ...data,
+    confirm_password:val,
+  });
+};
+
+
+const handleNewPasswprdChange=(val) =>{
+  setData({
+    ...data,
+    new_password:val,
+  });
+};
+
+
+  const Change=()=>{
+
+      const CurrentPassword = data.current_password
+      const NewPassword = data.new_password
+      const ConfirmPassword = data.confirm_password
+
+
+
+    const fbPassword= firestore()
+  .collection('users')
+  .doc(ID)
+  console.log(ID)
+
+  if (fbPassword==data.current_password){
+    if (data.password==data.confirm_password){
+      firestore()
+  .collection('users')
+  .doc(ID).update({
+    Password:NewPassword
+      })
+      }
+      
+
+  }
+     Alert.alert('Password has been changed'); 
+  }
+
+
   const {colors} = useTheme();
 
-  return (
-    <View style={styles.container}>
+  return ( 
+
+
+<View style={styles.container}>
               <View style={{...StyleSheet.absoluteFillObject,backgroundColor:'pink'}}></View>
        <View style={[styles.header,{backgroundColor:colors.background}]}>
        <Text style={styles.title}>Change Password</Text>
@@ -32,6 +100,8 @@ const ChangePasswordScreen= ({navigation}) => {
                     placeholder="Current Password."
                     style ={styles.textInput} 
                     autoCapitalize="none"
+                    onChangeText = {(val) => handleCurrentPassordChange(val)}
+
                     />
               </View>
 
@@ -42,6 +112,8 @@ const ChangePasswordScreen= ({navigation}) => {
                     placeholder="New Password."
                     style ={styles.textInput} 
                     autoCapitalize="none"
+                    onChangeText = {(val) => handleNewPasswprdChange(val)}
+
                     />
               </View>
               <View style ={styles.action}>
@@ -51,23 +123,30 @@ const ChangePasswordScreen= ({navigation}) => {
                     placeholder="Confirm New Password."
                     style ={styles.textInput} 
                     autoCapitalize="none"
+                    onChangeText = {(val) => handleConfirmPasswordChange(val)}
+
                     />
+
+                    
               </View>
 
+         
 
-              <View style = {[styles.button,{paddingTop:80}]}> 
-                  <TouchableOpacity style ={[styles.submit, {backgroundColor:'pink', borderRadius:75}]}>
+
+          </View>
+
+
+          
+          <View style={{flex:0.8}}>
+           
+          </View>
+<TouchableOpacity style={{marginBottom:50,}}onPress={() => Change()}
+                   style ={[styles.submit, {backgroundColor:'pink', borderRadius:75}]}>
                   
                       <Text style={styles.textsubmit} color ='grey'>Send</Text>
                   </TouchableOpacity>
-                 
-                  
-                  </View>
+              
 
-
-          </View>
-          <View style={{flex:0.5}}>
-          </View>
           </View>
            </View>
     </View>
@@ -81,8 +160,7 @@ container:{
   flex:1,
 },
 header:{
-  flex:1,
-  height:0.61*height,
+  flex:.6,
   borderBottomLeftRadius:75,
 },
 footer:{
@@ -98,7 +176,7 @@ title:{
   fontWeight:'bold',
   paddingStart:40,
   fontSize:30,
-  paddingTop:65,
+  paddingTop:20,
   color:'pink',
 },
 text_footer:{
