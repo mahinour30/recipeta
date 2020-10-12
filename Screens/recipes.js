fec270baf4d4e88ffcd96bd18171518354bfcc40
mahinour30 <mahinour1233@gmail.com>
2020-10-13 01:34:49 +0200

    Create recipes.js

---
+++ Screens/recipes.js
@@ -0,0 +1,99 @@
+export default recipes =[
+    {
+
+        id:1,
+        title:'Regular Cake',
+        caption:'enjoy a delicious easy to make home made cake in just 30 minutes',
+        photo: require('../Images/Cake.jpg'),
+        ingredients:'1 (18.25 ounce) package yellow cake mix \n1 (3 ounce) package instant lemon pudding mix \n1/3 cup orange juice \n1/2 cup vegetable oil \n4 eggs \n1 teaspoon lemon extract \n1/3 cup orange juice \n2/3 cup white sugar \n1/4 cup butter',
+        recipe:'Grease a 10 inch Bundt pan. Preheat oven to 325 degrees F (165 degrees C). \nIn a large bowl, stir together cake mix and pudding mix. Make a well in the center and pour in 3/4 cup orange juice, oil, eggs and lemon extract. Beat on low speed until blended. Scrape bowl, and beat 4 minutes on medium speed. Pour batter into prepared pan. \nBake in preheated oven for 50 to 60 minutes. Let cool in pan for 10 minutes, then turn out onto a wire rack and cool completely. \nIn a saucepan over medium heat, cook 1/3 cup orange juice, sugar and butter for two minutes. Drizzle over cake.',
+        survings:'10',
+    },
+    {
+
+        id:2,
+        title:'Pan Cakes',
+        caption:'step up your breakfast with these delicious pancakes',
+        photo: require('../Images/PanCake.jpg'),
+        ingredients:'1 ½ cups all-purpose flour \n3 ½ teaspoons baking powder \n1 teaspoon salt \n1 tablespoon white sugar \n1 ¼ cups milk \n1 egg \n3 tablespoons butter, melted',
+        recipe:'In a large bowl, sift together the flour, baking powder, salt and sugar. Make a well in the center and pour in the milk, egg and melted butter; mix until smooth. \nHeat a lightly oiled griddle or frying pan over medium high heat. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. Brown on both sides and serve hot.',
+        survings:'3',
+    },
+    {
+
+        id:3,
+        title:'Cheese Cake',
+        caption:'nothing brings people together like Cheesecakes!!',
+        photo: require('../Images/CheeseCake.jpg'),
+        ingredients:'1 (8 ounce) package cream cheese \n1 cup white sugar \n½ cup confectioners\' sugar \n1 cup sour cream \n¼ cup milk \n2 teaspoons vanilla extract \n3 large eggs \n1 cup sour cream \n¼ cup confectioners\' sugar \n1 teaspoon vanilla extract \n1 ½ cups fresh strawberries, halved \n1 cup fresh blueberries \n1 cup strawberry glaze',
+        recipe:'Preheat oven to 350 degrees F (175 degrees C.) \nIn a large bowl, beat the cream cheese, white sugar and 1/2 cup confectioners sugar until smooth. Beat in 1 cup sour cream, milk and 1 teaspoon vanilla. Slowly beat in eggs, one at a time. Pour filling into crust. \nBake in the preheated oven for 25 minutes, or until filling is set. Cool for 20 minutes. Preheat oven to 425 degrees F (220 degrees C.) \nIn a small bowl, combine 1 cup sour cream, 1/4 cup confectioners\' sugar and 1 teaspoon vanilla. Mix until smooth, then spread over cheesecake. Bake in preheated oven for 5 minutes. Allow to cool, then refrigerate for at least 1 hour. \nArrange strawberries and blueberries on top of the cheesecake. Spoon strawberry glaze over berries. Refrigerate several hours before serving.',
+        survings:'8',
+    },
+    {
+
+        id:4,
+        title:'Cookies',
+        caption:"feel you'r grandma's love with these yummy cookies",
+        photo: require('../Images/Cookies.jpg'),
+        ingredients:'¾ cup brown sugar \n½ cup unsalted butter, at room temperature \n½ cup white sugar \n1 teaspoon salt \n1 large egg \n1 teaspoon vanilla extract \n1 ½ cups all-purpose flour \n¾ teaspoon baking soda \n1 cup milk chocolate chips \n1 (3 ounce) bar dark chocolate, cut into chunks \n3 tablespoons white chocolate chips',
+        recipe:'Combine brown sugar, butter, white sugar, and salt in a large bowl; beat with an electric mixer until a creamy, deep brown mixture forms. Add egg and vanilla; beat until mixture lightens and becomes smooth, 10 to 15 seconds. \nMix flour and baking soda together in a separate bowl. Add slowly to the wet ingredients until mostly incorporated, but some white traces of flour remain. Fold in milk chocolate, dark chocolate, and white chocolate using a spatula, not the mixer. Cover the dough and refrigerate for at least 30 minutes, or up to 48 hours. \nPreheat the oven to 350 degrees F (175 degrees C) when ready to bake. \nDivide cold dough into 2 1/4-ounce portions and place on a cookie sheet. \nBake in the preheated oven until the edges start to look golden brown and crispy, 10 to 12 minutes. Do not overbake; the centers will not look fully done. Cool until cookies are set, about 30 minutes.',
+        survings:'12',
+
+    },
+    {
+
+        id:5,
+        title:'Apple Pie',
+        caption:'nothing else feel like love',
+        photo: require('../Images/ApplePie.jpg'),
+        ingredients:'1 recipe pastry for a 9 inch double crust pie \n½ cup unsalted butter \n3 tablespoons all-purpose flour \n¼ cup water \n½ cup white sugar \n½ cup packed brown sugar \n8 medium (2-3/4" dia) (approx 3 per lb)s Granny Smith apples - peeled, cored and sliced',
+        recipe:'Preheat oven to 425 degrees F (220 degrees C). Melt the butter in a saucepan. Stir in flour to form a paste. Add water, white sugar and brown sugar, and bring to a boil. Reduce temperature and let simmer. \nPlace the bottom crust in your pan. Fill with apples, mounded slightly. Cover with a lattice work crust. Gently pour the sugar and butter liquid over the crust. Pour slowly so that it does not run off. \nBake 15 minutes in the preheated oven. Reduce the temperature to 350 degrees F (175 degrees C). Continue baking for 35 to 45 minutes, until apples are soft.',
+        survings:'8',
+
+    },
+    {
+
+        id:6,
+        title:'Cup Cake',
+        caption:'have you tried this easy recipe yet!!',
+        photo: require('../Images/CupCake.jpg'),
+        ingredients:'1 ½ cups sifted all-purpose flour \n1 ½ teaspoons baking powder \n¼ teaspoon salt \n1 cup white sugar\n½ cup virgin coconut oil \n3 tablespoons Key lime juice \n1 ½ teaspoons Key lime zest \n1 teaspoon vanilla extract \n2 large eggs eggs \n½ cup milk ',
+        recipe:'Preheat the oven to 350 degrees F (175 degrees C). Grease a 12-cup muffin tin. \nCombine flour, baking powder, and salt in a bowl. \nBeat white sugar and 1/2 cup coconut oil in a large bowl using an electric mixer until light and airy, 3 to 4 minutes. Add 3 tablespoons Key lime juice, 1 1/2 teaspoons Key lime zest, and vanilla extract. Add eggs 1 at a time, beating well after each addition. \nReduce mixer speed to low. Beat in 1/3 of the flour mixture at a time, alternating with 1/2 the milk and ending with the last 1/3 of the flour mixture. Mix until just combined; do not overmix. Pour batter into the muffin cups until about 2/3-full. \nBake in the preheated oven until tops spring back when lightly pressed, 20 to 25 minutes. Cool in the tin for 5 minutes. Transfer to a wire rack and let cool completely, about 20 minutes. \nBeat cream cheese and 1/4 cup coconut oil together in a bowl using an electric mixer until creamy and well combined. Mix in 1 1/2 teaspoons Key lime juice and 1/2 teaspoon Key lime zest. Gradually stir in confectioners\' sugar. Frost cooled cupcakes. \nHeat a nonstick skillet over medium heat. Cook and stir the flaked coconut until lightly toasted, about 4 minutes. Remove from pan to cool, about 5 minutes. Top frosted cupcakes with the coconut, pressing in lightly.',
+        survings:'12',
+
+    },
+    {
+
+        id:7,
+        title:'Lemon Tart',
+        caption:'the best summer dessert',
+        photo: require('../Images/LemonTart.jpg'),
+        ingredients:'2 ½ cups all-purpose flour \n⅞ cup butter \n¾ cup confectioners\' sugar \n1 egg yolk \n1 ½ tablespoons cold water \n4 large eggs eggs \n¾ cup superfine sugar (or granulated sugar, processed to fine texture in a food processor) \n2 tablespoons minced lemon zest \n¾ cup fresh lemon juice \n½ cup heavy whipping cream',
+        recipe:'Place flour, butter, and confectioners\' sugar in food processor; process until mixture resembles bread crumbs. Add egg yolk and 1 to 2 tablespoons cold water, and process until mixture forms a dough. Mold dough into 2 flat shapes, wrap in plastic wrap, and chill for 20 minutes. \nRoll out pastry to 1/8 inch thick (3mm), and line a 9- or 10-inch (24 cm) tart pan. Patch any cracks with extra dough and chill the crust in the freezer for 20 minutes. \nPreheat oven to 350 degrees F (175 degrees C). \nLine pastry with foil, fill the foil with a little raw rice, and bake in preheated oven for 15 minutes. Remove foil and rice, and continue baking until pastry is a light golden color. \nIn a mixing bowl, mix together eggs, superfine sugar, lemon zest, lemon juice, and cream. Do not over beat. Pour the mixture through a strainer. Place the tart pan on a baking sheet, and carefully pour the lemon mixture into the baked pastry crust. \nBake until the tart is nearly set, about 20 minutes. The filling will be runny in the center, and will set when cool; don\'t over cook, or cracks will appear. Cool and serve.',
+        survings:'8',
+
+    },
+    {
+
+        id:8,
+        title:'Pigs In Planket',
+        caption:'enjoy this hot snak in just 20 minutes!',
+        photo: require('../Images/PigsInPlanket.jpg'),
+        ingredients:'8 frankfurter (5 in long x 3/4 in dia, 10 per pound)s frankfurters \n8 slices American processed cheese \n1 (10 ounce) package refrigerated biscuit dough',
+        recipe:'Preheat oven to 350 degrees F (175 degrees C). \nWrap cheese around each frankfurter then the biscuit around that. Put on cookie sheet with the overlap of biscuit faced down, so you don\'t have to use tooth picks. \nBake in preheated oven until biscuits are brown, about 10 to 15 minutes.',
+        survings:'6',
+
+    },
+    {
+
+        id:9,
+        title:'Muffins',
+        caption:'easiest muffin recipie for the whole family to enjoy',
+        photo: require('../Images/Muffins.jpg'),
+        ingredients:'½ cup unsalted butter \n¾ cup chopped dried sweetened pineapple \n1 cup all-purpose flour \n1 cup yellow cornmeal \n½ teaspoon baking soda \n½ teaspoon salt \n1 cup buttermilk \n2 large eggs',
+        recipe:'Preheat oven to 375 degrees F (190 degrees C). Line a muffin pan with paper muffin liners, and lightly spray liners with cooking spray. \nMelt butter in a heavy saucepan over medium heat until golden brown. Remove from heat and stir in pineapple. Set aside to steep for 20 minutes. \nWhisk flour, cornmeal, baking soda, and salt until combined. Set aside. \nCombine buttermilk, eggs, and pineapple mixture in a large bowl. Whisk until smooth, about 1 minute. Add flour mixture to buttermilk mixture; whisk until just combined. \nDivide batter evenly between the prepared muffin cups and bake in the preheated oven until a toothpick inserted in the center of a muffin comes out clean, about 25 minutes. Cool in the pan for 5 minutes before removing to cool completely on a wire rack.',
+        survings:'12',
+
+    },
+
+];
